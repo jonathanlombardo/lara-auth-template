63b111a01b0d117e049729b6c0ec7c7324cfdd76
Jonathan <149710165+jonathanlombardo@users.noreply.github.com>
2024-04-02 13:39:07 +0200

    Initial commit

---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,11 @@
+<header class="py-3 bg-primary text-white">
+  <div class="container d-flex justify-content-between align-items-center">
+
+    <span>{{env("APP_NAME", "NewProject")}} Header</span>
+    <nav>
+      <ul class="d-flex align-items-center gap-3">
+        <li><a href="{{route("home")}}">Home</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
